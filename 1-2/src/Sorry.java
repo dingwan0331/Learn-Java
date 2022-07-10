28406ee5edc243768034520f2db9a3bb9bc0277e
dingwan <dingwan0331@gmail.com>
2022-07-11 02:06:20 +0900

    day2

--- 1-2/src/Sorry.java
+++ 1-2/src/Sorry.java
@@ -2,7 +2,8 @@
 public class Sorry {
 
 	public static void main(String[] args) {
-		System.out.println("Sorry~~\n재밌어서 미안합니다~~");
+		System.out.println("Sorry~~");
+		System.out.println("재밌어서 미안합니다");
 	}
 
 }
